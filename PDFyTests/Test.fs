e1231916dbeb349a0c62b23ede3fd38606fd753e
Ashley Towns <ashleyis@me.com>
2013-03-22 09:15:10 +1100

    updated tests

--- PDFyTests/Test.fs
+++ PDFyTests/Test.fs
@@ -6,13 +6,24 @@ open NUnit.Framework
 open FsUnit
 
 [<TestFixture>]
-type ``Given a pdf file with annotations`` () = 
-    let pdfdoc = PDFy.PDFDocument.openDocument "Test1.pdf"
+type ``Given a pdf file without annotations`` () = 
+    do
+        cocoaInit()
     
+    let pdfdoc = PDFy.PDFDocument.openDocument "Test1.pdf"
+
     [<Test>] 
-    member x.``when i try to open an existing file it returns a pdfDocument`` ()=
+    member x.``when i try to open an existing file it returns a pdfDocument`` () =
         pdfdoc.GetType() = typeof<MonoMac.PdfKit.PdfDocument> |> should be True
     
     [<Test>]
     member x.``a document should contain at least one page`` () =
         pdfdoc.PageCount |> should be (greaterThanOrEqualTo 1)
+
+    [<Test>]
+    member x.``Annotations should return a List<PDFAnnotation> of zero`` () =
+        (getAnnotationsFromDocument pdfdoc).GetType() = typeof<List<PDFAnnotation>> 
+        |> should be True
+        
+        List.length (getAnnotationsFromDocument pdfdoc)
+        |> should equal 0

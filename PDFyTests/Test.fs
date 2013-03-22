5c2c2c6d30c3421b3742c7dad4b9bf1b9b43f8ba
Ashley Towns <ashleyis@me.com>
2013-03-22 20:00:54 +1100

    updated tests

--- PDFyTests/Test.fs
+++ PDFyTests/Test.fs
@@ -5,15 +5,22 @@ open PDFy
 open NUnit.Framework
 open FsUnit
 
-[<TestFixture>]
-type ``Given a pdf file without annotations`` () = 
-    do
-        cocoaInit()
+[<SetUpFixture>]
+type Config () =
+    [<SetUp>]
+    member x.Setup () =
+        cocoaInit() 
     
-    let pdfdoc = PDFy.PDFDocument.openDocument "Test1.pdf"
+    [<TearDown>]
+    member x.TearDown () =
+        ()
+
+[<TestFixture>]
+type ``Given a pdf file without annotations`` () =     
+    let pdfdoc = PDFy.PDFDocument.openDocument "TestAssets/Test1.pdf"
 
     [<Test>] 
-    member x.``when i try to open an existing file it returns a pdfDocument`` () =
+    member x.``when I try to open an existing file it returns a pdfDocument`` () =
         pdfdoc.GetType() = typeof<MonoMac.PdfKit.PdfDocument> |> should be True
     
     [<Test>]
@@ -27,3 +34,50 @@ type ``Given a pdf file without annotations`` () =
         
         List.length (getAnnotationsFromDocument pdfdoc)
         |> should equal 0
+
+[<TestFixture>]
+type ``Given a pdf file with some annotations`` () =     
+    let pdfdoc = PDFy.PDFDocument.openDocument "TestAssets/Test2.pdf"
+    
+    [<Test>]
+    member x.``when I try to open an existing file it returns a pdfDocument`` () = 
+        pdfdoc.GetType() = typeof<MonoMac.PdfKit.PdfDocument> |> should be True
+
+    [<Test>]
+    member x.``a document should contain at least one page`` () =
+        pdfdoc.PageCount |> should be (greaterThanOrEqualTo 1)
+
+    [<Test>]
+    member x.``Annotations should return a List<PDFAnnotation> of annotations`` () =
+        (getAnnotationsFromDocument pdfdoc).GetType() = typeof<List<PDFAnnotation>> 
+        |> should be True
+        
+        List.length (getAnnotationsFromDocument pdfdoc)
+        |> should be (greaterThanOrEqualTo 1)
+    
+    [<Test>]
+    member x.``I should be able to filter annotations by a string`` () =
+        (getAnnotationsFromDocument pdfdoc) 
+        |> List.filter(PDFAnnotation.hotspotNamed "TestHotspot")
+        |> List.length
+        |> should be (greaterThanOrEqualTo 1)        
+        
+    [<Test>]
+    member x.``I should be able to filter annotations by a regex`` () =
+        (getAnnotationsFromDocument pdfdoc) 
+        |> List.filter(PDFAnnotation.hotspotRegex (new System.Text.RegularExpressions.Regex("^TestHotspot$")))
+        |> List.length
+        |> should be (greaterThanOrEqualTo 1) 
+    
+    [<Test>]
+    member x.``I Should be able to rename a hotspot`` () = 
+        let annotation = 
+            (getAnnotationsFromDocument pdfdoc) 
+            |> List.find(PDFAnnotation.hotspotNamed "TestHotspot")
+        annotation.hotspot <- "NewHotspot"
+        
+        let result = 
+            (getAnnotationsFromDocument pdfdoc) 
+            |> List.tryFind(PDFAnnotation.hotspotNamed "NewHotspot")
+        result.IsSome |> should be True
+        
